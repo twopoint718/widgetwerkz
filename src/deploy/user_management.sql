07e4d1891a04474aefabaffc579e313eb271cea3
Chris Wilson <cwilson@8thlight.com>
2018-12-12 16:34:15 -0600

    basic_auth -> auth

--- src/deploy/user_management.sql
+++ src/deploy/user_management.sql
@@ -5,7 +5,7 @@ BEGIN;
 --------------------------------------------------------------------------------
 -- SCHEMA/EXTENSIONS
 
-create schema if not exists basic_auth;
+create schema if not exists auth;
 create extension if not exists pgcrypto; -- password hashing
 create extension if not exists pgjwt;    -- generate JWT tokens
 
@@ -13,7 +13,7 @@ create extension if not exists pgjwt;    -- generate JWT tokens
 --------------------------------------------------------------------------------
 -- TYPES
 
-create type basic_auth.jwt_token as (
+create type auth.jwt_token as (
   token text
 );
 
@@ -21,11 +21,11 @@ create type basic_auth.jwt_token as (
 --------------------------------------------------------------------------------
 -- USER ROLES
 
--- We put things inside the basic_auth schema to hide
+-- We put things inside the auth schema to hide
 -- them from public view. Certain public procs/views will
 -- refer to helpers and tables inside.
 create table if not exists
-basic_auth.users (
+auth.users (
     email    text primary key check ( email ~* '^.+@.+\..+$' ),
     pass     text not null check (length(pass) < 512),
     role     name not null check (length(role) < 512)
@@ -34,7 +34,7 @@ basic_auth.users (
 -- Ideally this would be a FK against the 'pg_roles' table. Instead, we'll use
 -- a trigger to check that a users' role exists as an actual DB role.
 create or replace function
-basic_auth.check_role_exists() returns trigger as $$
+auth.check_role_exists() returns trigger as $$
 begin
   if not exists (select 1 from pg_roles as r where r.rolname = new.role) then
     raise foreign_key_violation using message =
@@ -46,9 +46,9 @@ end
 $$ language plpgsql;
 
 create constraint trigger ensure_user_role_exists
-    after insert or update on basic_auth.users
+    after insert or update on auth.users
     for each row
-    execute procedure basic_auth.check_role_exists();
+    execute procedure auth.check_role_exists();
 
 
 --------------------------------------------------------------------------------
@@ -56,7 +56,7 @@ create constraint trigger ensure_user_role_exists
 
 -- Salt and blowfish hash a new or changed user password
 create or replace function
-basic_auth.encrypt_pass() returns trigger as $$
+auth.encrypt_pass() returns trigger as $$
 begin
     if tg_op = 'INSERT' or new.pass <> old.pass then
         new.pass = crypt(new.pass, gen_salt('bf'));
@@ -73,12 +73,12 @@ create trigger encrypt_pass
 
 -- Check encrypted password, returns role if password OK
 create or replace function
-basic_auth.user_role(email text, pass text) returns name
+auth.user_role(email text, pass text) returns name
   language plpgsql
   as $$
 begin
   return (
-    select role from basic_auth.users
+    select role from auth.users
     where users.email = user_role.email
     and users.pass = crypt(user_role.pass, users.pass)
   );
@@ -96,10 +96,10 @@ create or replace function
 login(email text, pass text) returns basic_auth.jwt_token as $$
 declare
   _role name;
-  result basic_auth.jwt_token;
+  result auth.jwt_token;
 begin
   -- check email and password
-  select basic_auth.user_role(email, pass) into _role;
+  select auth.user_role(email, pass) into _role;
   if _role is null then
     raise invalid_password using message = 'invalid user or password';
   end if;
